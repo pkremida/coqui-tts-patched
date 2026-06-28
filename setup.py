957ced87bf1474346ff1a7158921536135d6f604
Peter <pkremida@gmail.com>
2026-06-27 18:07:26 -0700

    Allow Python 3.12 (change version check from < 3.12 to < 3.13)

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
 
 python_version = sys.version.split()[0]
-if Version(python_version) < Version("3.9") or Version(python_version) >= Version("3.12"):
-    raise RuntimeError("TTS requires python >= 3.9 and < 3.12 " "but your Python version is {}".format(sys.version))
+if Version(python_version) < Version("3.9") or Version(python_version) >= Version("3.13"):
+    raise RuntimeError("TTS requires python >= 3.9 and < 3.13 " "but your Python version is {}".format(sys.version))
 
 
 cwd = os.path.dirname(os.path.abspath(__file__))
